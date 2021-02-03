9e5d3702825a41827d140bea7a9a57fc83c5e491
Wendell Rocha <wendellrocha@outlook.com>
2021-02-03 13:16:53 -0300

    step by step

--- lib/ex_mon_api/trainer.ex
+++ lib/ex_mon_api/trainer.ex
@@ -1,4 +1,4 @@
-defmodule ExMon.Trainer do
+defmodule ExMonApi.Trainer do
   use Ecto.Schema
 
   import Ecto.Changeset
@@ -8,13 +8,28 @@ defmodule ExMon.Trainer do
   schema "trainers" do
     field :name, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
     timestamps()
   end
 
-  @required_params [:name, :password_hash]
+  @required_params [:name, :password]
+
+  def buid(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
+    |> put_pass_hash()
   end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
 end
